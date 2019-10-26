8346b81070a227bc940e515f4bf36a4b984e63b2
Jayden-Liang <liangjisong@foxmail.com>
2019-10-26 17:50:36 +0800

    Updates

---
+++ precache-manifest.10e735417ab25b74dd90cffd1c2aa654.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e541f30fd2948d47a64fb0f343ce46",
+    "url": "/simulate_airbnb/index.html"
+  },
+  {
+    "revision": "9647ab696bc70b5a0d05",
+    "url": "/simulate_airbnb/static/css/main.bdbb1cb7.chunk.css"
+  },
+  {
+    "revision": "126c5fa35acf8147024a",
+    "url": "/simulate_airbnb/static/js/2.8d946cc4.chunk.js"
+  },
+  {
+    "revision": "9647ab696bc70b5a0d05",
+    "url": "/simulate_airbnb/static/js/main.36009d1c.chunk.js"
+  },
+  {
+    "revision": "0b0e3b2b4930b388ffe3",
+    "url": "/simulate_airbnb/static/js/runtime-main.74e60c2a.js"
+  },
+  {
+    "revision": "74607759a3d371583662a28d663ee3bf",
+    "url": "/simulate_airbnb/static/media/banner_1.74607759.jpg"
+  },
+  {
+    "revision": "4ff2948f9750627f36e1f3bc8b912efc",
+    "url": "/simulate_airbnb/static/media/banner_2.4ff2948f.jpg"
+  },
+  {
+    "revision": "92362d4d175b211828ad3761e7617b27",
+    "url": "/simulate_airbnb/static/media/banner_3.92362d4d.jpg"
+  },
+  {
+    "revision": "cc25ab2011081645d6d6100ded6d3980",
+    "url": "/simulate_airbnb/static/media/banner_4.cc25ab20.jpg"
+  },
+  {
+    "revision": "302c047c32e3ac0f6fba446589407abe",
+    "url": "/simulate_airbnb/static/media/plus.302c047c.jpg"
+  }
+]);
